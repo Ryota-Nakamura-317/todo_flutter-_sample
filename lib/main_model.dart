a984c06feb5cdc54103903ac1f99c9c1f9af31c0
Ryota-Nakamura-317 <nkmr94317rt@gmail.com>
2021-01-04 23:00:04 +0900

    チェックボックス 削除、完了と複数削除可能対応

--- lib/main_model.dart
+++ lib/main_model.dart
@@ -36,4 +36,27 @@ class MainModel extends ChangeNotifier {
       'createdAt': Timestamp.now(),
     });
   }
+
+  void reload() {
+    notifyListeners();
+  }
+
+  Future deleteCheckedItems() async {
+    final checkedItems = todoList.where((todo) => todo.isDone).toList();
+    final references =
+        checkedItems.map((todo) => todo.documentReference).toList();
+
+    final batch = FirebaseFirestore.instance.batch();
+
+    references.forEach((reference) {
+      batch.delete(reference);
+    });
+
+    return batch.commit();
+  }
+
+  bool checkedShouldActiveCompleteButton() {
+    final checkedItems = todoList.where((todo) => todo.isDone).toList();
+    return checkedItems.length > 0;
+  }
 }
